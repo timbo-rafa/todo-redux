18d31a17c3cf264af08066e7818ed7d0f7783067
Rafael Timbó <timbo.rafa@gmail.com>
2023-07-27 13:06:07 -0400

    Updates

---
+++ server/app/page.js
@@ -0,0 +1,549 @@
+(() => {
+var exports = {};
+exports.id = 931;
+exports.ids = [931];
+exports.modules = {
+
+/***/ 18038:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/compiled/react");
+
+/***/ }),
+
+/***/ 98704:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/compiled/react-dom/server-rendering-stub");
+
+/***/ }),
+
+/***/ 97897:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/compiled/react-server-dom-webpack/client");
+
+/***/ }),
+
+/***/ 56786:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/compiled/react/jsx-runtime");
+
+/***/ }),
+
+/***/ 3280:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/app-router-context.js");
+
+/***/ }),
+
+/***/ 69274:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/hooks-client-context.js");
+
+/***/ }),
+
+/***/ 11751:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/add-path-prefix.js");
+
+/***/ }),
+
+/***/ 21668:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/handle-smooth-scroll.js");
+
+/***/ }),
+
+/***/ 1897:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/is-bot.js");
+
+/***/ }),
+
+/***/ 28854:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/parse-path.js");
+
+/***/ }),
+
+/***/ 93297:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/router/utils/remove-trailing-slash.js");
+
+/***/ }),
+
+/***/ 3349:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/server-inserted-html.js");
+
+/***/ }),
+
+/***/ 20737:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "AppRouter": () => (/* reexport default from dynamic */ next_dist_client_components_app_router__WEBPACK_IMPORTED_MODULE_0___default.a),
+/* harmony export */   "GlobalError": () => (/* reexport default from dynamic */ next_dist_client_components_error_boundary__WEBPACK_IMPORTED_MODULE_3___default.a),
+/* harmony export */   "LayoutRouter": () => (/* reexport default from dynamic */ next_dist_client_components_layout_router__WEBPACK_IMPORTED_MODULE_1___default.a),
+/* harmony export */   "RenderFromTemplateContext": () => (/* reexport default from dynamic */ next_dist_client_components_render_from_template_context__WEBPACK_IMPORTED_MODULE_2___default.a),
+/* harmony export */   "__next_app_webpack_require__": () => (/* binding */ __next_app_webpack_require__),
+/* harmony export */   "pages": () => (/* binding */ pages),
+/* harmony export */   "renderToReadableStream": () => (/* reexport safe */ next_dist_compiled_react_server_dom_webpack_server_edge__WEBPACK_IMPORTED_MODULE_7__.renderToReadableStream),
+/* harmony export */   "requestAsyncStorage": () => (/* reexport safe */ next_dist_client_components_request_async_storage__WEBPACK_IMPORTED_MODULE_5__.requestAsyncStorage),
+/* harmony export */   "serverHooks": () => (/* reexport module object */ next_dist_client_components_hooks_server_context__WEBPACK_IMPORTED_MODULE_6__),
+/* harmony export */   "staticGenerationAsyncStorage": () => (/* reexport safe */ next_dist_client_components_static_generation_async_storage__WEBPACK_IMPORTED_MODULE_4__.staticGenerationAsyncStorage),
+/* harmony export */   "tree": () => (/* binding */ tree)
+/* harmony export */ });
+/* harmony import */ var next_dist_client_components_app_router__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2286);
+/* harmony import */ var next_dist_client_components_app_router__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(next_dist_client_components_app_router__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_dist_client_components_layout_router__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(65717);
+/* harmony import */ var next_dist_client_components_layout_router__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_dist_client_components_layout_router__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var next_dist_client_components_render_from_template_context__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(88353);
+/* harmony import */ var next_dist_client_components_render_from_template_context__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(next_dist_client_components_render_from_template_context__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var next_dist_client_components_error_boundary__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(7340);
+/* harmony import */ var next_dist_client_components_error_boundary__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(next_dist_client_components_error_boundary__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var next_dist_client_components_static_generation_async_storage__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(79029);
+/* harmony import */ var next_dist_client_components_static_generation_async_storage__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(next_dist_client_components_static_generation_async_storage__WEBPACK_IMPORTED_MODULE_4__);
+/* harmony import */ var next_dist_client_components_request_async_storage__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(32458);
+/* harmony import */ var next_dist_client_components_request_async_storage__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(next_dist_client_components_request_async_storage__WEBPACK_IMPORTED_MODULE_5__);
+/* harmony import */ var next_dist_client_components_hooks_server_context__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(94065);
+/* harmony import */ var next_dist_client_components_hooks_server_context__WEBPACK_IMPORTED_MODULE_6___default = /*#__PURE__*/__webpack_require__.n(next_dist_client_components_hooks_server_context__WEBPACK_IMPORTED_MODULE_6__);
+/* harmony import */ var next_dist_compiled_react_server_dom_webpack_server_edge__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(68378);
+
+    const tree = {
+        children: [
+        '',
+        {
+        children: ['', {}, {
+          page: [() => Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 83868, 23)), "/Users/rafael/code/todo-redux/src/app/page.tsx"],
+          
+        }]
+      },
+        {
+          'layout': [() => Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 14838)), "/Users/rafael/code/todo-redux/src/app/layout.tsx"],
+          metadata: {
+    icon: [() => Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 51656))],
+    apple: [],
+    opengraph: [],
+    twitter: [],
+  }
+        }
+      ]
+      }.children;
+    const pages = ["/Users/rafael/code/todo-redux/src/app/page.tsx"];
+
+    
+    
+    
+    
+
+    
+    
+    
+
+    
+
+    
+    const __next_app_webpack_require__ = __webpack_require__
+  
+
+/***/ }),
+
+/***/ 61328:
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 48950, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 12513, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 81551, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 95958, 23))
+
+/***/ }),
+
+/***/ 97156:
+/***/ (() => {
+
+
+
+/***/ }),
+
+/***/ 5240:
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 82924))
+
+/***/ }),
+
+/***/ 19598:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"main": "page_main__ibFHK",
+	"description": "page_description__s_Lqk",
+	"code": "page_code__Cdcue",
+	"grid": "page_grid__2WZXq",
+	"card": "page_card__ftWzl",
+	"center": "page_center__GvJ9Y",
+	"logo": "page_logo__M5piD",
+	"thirteen": "page_thirteen__DLEbK",
+	"rotate": "page_rotate__P15uU",
+	"content": "page_content__rFejU",
+	"vercelLogo": "page_vercelLogo__1QD2W"
+};
+
+
+/***/ }),
+
+/***/ 14838:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ RootLayout),
+/* harmony export */   "metadata": () => (/* binding */ metadata)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(54458);
+/* harmony import */ var _globals_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(75553);
+/* harmony import */ var _globals_css__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_globals_css__WEBPACK_IMPORTED_MODULE_1__);
+
+
+const metadata = {
+    title: "Create Next App",
+    description: "Generated by create next app"
+};
+function RootLayout({ children  }) {
+    return /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("html", {
+        lang: "en",
+        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("body", {
+            children: children
+        })
+    });
+}
+
+
+/***/ }),
+
+/***/ 83868:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+/* __next_internal_client_entry_do_not_use__ default */ const { createProxy  } = __webpack_require__(21399);
+module.exports = createProxy("/Users/rafael/code/todo-redux/src/app/page.tsx");
+
+
+/***/ }),
+
+/***/ 51656:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({"url":"/_next/static/media/metadata/favicon.603d046c.ico","type":"image/x-icon","sizes":"any"});
+
+/***/ }),
+
+/***/ 82924:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ Home)
+});
+
+// EXTERNAL MODULE: external "next/dist/compiled/react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(56786);
+// EXTERNAL MODULE: ./node_modules/react-redux/lib/index.js
+var lib = __webpack_require__(1560);
+// EXTERNAL MODULE: ./node_modules/@mui/material/node/Card/index.js
+var Card = __webpack_require__(76395);
+var Card_default = /*#__PURE__*/__webpack_require__.n(Card);
+// EXTERNAL MODULE: ./node_modules/@mui/material/node/index.js
+var node = __webpack_require__(64085);
+// EXTERNAL MODULE: ./node_modules/@mui/material/node/Button/index.js
+var Button = __webpack_require__(98511);
+var Button_default = /*#__PURE__*/__webpack_require__.n(Button);
+// EXTERNAL MODULE: external "next/dist/compiled/react"
+var react_ = __webpack_require__(18038);
+var react_default = /*#__PURE__*/__webpack_require__.n(react_);
+;// CONCATENATED MODULE: ./src/app/features/todo/AddTodo.tsx
+
+
+
+
+function AddTodo({ onNewTodo  }) {
+    const [todoText, setTodoText] = react_default().useState("");
+    const handleChange = react_default().useCallback((event)=>{
+        setTodoText(event.target.value);
+    }, [
+        setTodoText
+    ]);
+    const handleClick = react_default().useCallback(()=>{
+        onNewTodo(todoText);
+    }, [
+        onNewTodo,
+        todoText
+    ]);
+    return /*#__PURE__*/ (0,jsx_runtime_.jsxs)(jsx_runtime_.Fragment, {
+        children: [
+            /*#__PURE__*/ jsx_runtime_.jsx(node.Box, {
+                component: "form",
+                sx: {
+                    "& > :not(style)": {
+                        m: 1,
+                        width: "25ch"
+                    }
+                },
+                noValidate: true,
+                autoComplete: "off",
+                children: /*#__PURE__*/ jsx_runtime_.jsx(node.TextField, {
+                    value: todoText,
+                    onChange: handleChange
+                })
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx((Button_default()), {
+                onClick: handleClick,
+                children: "Add Todo "
+            })
+        ]
+    });
+}
+
+// EXTERNAL MODULE: ./node_modules/@mui/material/node/List/index.js
+var List = __webpack_require__(54436);
+var List_default = /*#__PURE__*/__webpack_require__.n(List);
+// EXTERNAL MODULE: ./node_modules/@mui/material/node/Typography/index.js
+var Typography = __webpack_require__(43360);
+var Typography_default = /*#__PURE__*/__webpack_require__.n(Typography);
+// EXTERNAL MODULE: ./node_modules/@mui/material/node/Checkbox/index.js
+var Checkbox = __webpack_require__(63754);
+var Checkbox_default = /*#__PURE__*/__webpack_require__.n(Checkbox);
+;// CONCATENATED MODULE: ./src/app/features/todo/TodoItem.tsx
+
+
+
+function TodoItem({ id , isCompleted , text  }) {
+    return /*#__PURE__*/ (0,jsx_runtime_.jsxs)(jsx_runtime_.Fragment, {
+        children: [
+            /*#__PURE__*/ jsx_runtime_.jsx((Typography_default()), {
+                children: id
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx((Checkbox_default()), {
+                checked: isCompleted
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx((Typography_default()), {
+                children: text
+            })
+        ]
+    });
+}
+
+;// CONCATENATED MODULE: ./src/app/store/hooks.ts
+
+const useAppDispatch = lib.useDispatch;
+const useAppSelector = lib.useSelector;
+
+;// CONCATENATED MODULE: ./src/app/features/todo/todo.state.hook.ts
+
+function useTodoState() {
+    return useAppSelector((state)=>state.todo.todoList);
+}
+
+;// CONCATENATED MODULE: ./src/app/features/todo/TodoList.tsx
+
+
+
+
+
+
+function TodoList() {
+    const todos = useTodoState();
+    const todoListContent = react_default().useMemo(()=>todos.map((todo)=>/*#__PURE__*/ jsx_runtime_.jsx(TodoItem, {
+                ...todo
+            }, todo.id)), [
+        todos
+    ]);
+    if (todos.length === 0) {
+        return /*#__PURE__*/ jsx_runtime_.jsx((Typography_default()), {
+            children: " No Todos "
+        });
+    }
+    return /*#__PURE__*/ jsx_runtime_.jsx((List_default()), {
+        children: todoListContent
+    });
+}
+
+// EXTERNAL MODULE: ./node_modules/@reduxjs/toolkit/dist/redux-toolkit.cjs.production.min.js
+var redux_toolkit_cjs_production_min = __webpack_require__(10668);
+;// CONCATENATED MODULE: ./src/app/features/todo/todoSlice.ts
+
+const initialState = {
+    todoList: []
+};
+const todoSlice = (0,redux_toolkit_cjs_production_min.createSlice)({
+    name: "todo",
+    initialState,
+    reducers: {
+        // Redux Toolkit allows us to write "mutating" logic in reducers. It
+        // doesn't actually mutate the state because it uses the Immer library,
+        // which detects changes to a "draft state" and produces a brand new
+        // immutable state based off those changes
+        add: (state, action)=>{
+            state.todoList.push(action.payload);
+        },
+        remove: (state, action)=>{
+            state.todoList = state.todoList.filter((todoItem)=>todoItem.id === action.payload);
+        },
+        markAsComplete: (state, action)=>{
+            const todoItem = state.todoList.find((todoItem)=>todoItem.id === action.payload);
+            if (todoItem) {
+                todoItem.isCompleted = true;
+            }
+        }
+    }
+});
+// Action creators are generated for each case reducer function
+const todoActions = todoSlice.actions;
+const todoReducer = todoSlice.reducer;
+
+;// CONCATENATED MODULE: ./src/app/features/todo/create-todo.hook.ts
+function useCreateTodo() {
+    return api.createTodo;
+}
+let id = 1;
+const api = {
+    createTodo: (todo)=>{
+        return new Promise((resolve)=>{
+            const newId = id;
+            id++;
+            setTimeout(()=>resolve({
+                    id: String(newId),
+                    text: todo.text,
+                    isCompleted: todo.isCompleted || false
+                }), 500);
+        });
+    }
+};
+
+;// CONCATENATED MODULE: ./src/app/features/todo/TodoCard.tsx
+
+
+
+
+
+
+
+
+
+function TodoCard() {
+    const dispatch = useAppDispatch();
+    const createTodo = useCreateTodo();
+    const handleNewTodo = react_default().useCallback(async (text)=>{
+        const createdTodo = await createTodo({
+            text
+        });
+        dispatch(todoActions.add(createdTodo));
+    }, [
+        createTodo,
+        dispatch
+    ]);
+    return /*#__PURE__*/ jsx_runtime_.jsx((Card_default()), {
+        children: /*#__PURE__*/ (0,jsx_runtime_.jsxs)(node.CardContent, {
+            children: [
+                /*#__PURE__*/ jsx_runtime_.jsx(TodoList, {}),
+                /*#__PURE__*/ jsx_runtime_.jsx(AddTodo, {
+                    onNewTodo: handleNewTodo
+                })
+            ]
+        })
+    });
+}
+
+;// CONCATENATED MODULE: ./src/app/features/todo/index.ts
+
+
+
+
+// EXTERNAL MODULE: ./src/app/page.module.css
+var page_module = __webpack_require__(19598);
+var page_module_default = /*#__PURE__*/__webpack_require__.n(page_module);
+;// CONCATENATED MODULE: ./src/app/store/store.ts
+
+
+const store = (0,redux_toolkit_cjs_production_min.configureStore)({
+    reducer: {
+        todo: todoReducer
+    }
+});
+
+;// CONCATENATED MODULE: ./src/app/store/index.ts
+
+
+;// CONCATENATED MODULE: ./src/app/page.tsx
+
+
+
+
+
+
+function Home() {
+    return /*#__PURE__*/ jsx_runtime_.jsx("main", {
+        className: (page_module_default()).main,
+        children: /*#__PURE__*/ jsx_runtime_.jsx(lib.Provider, {
+            store: store,
+            children: /*#__PURE__*/ jsx_runtime_.jsx(TodoCard, {})
+        })
+    });
+}
+
+
+/***/ }),
+
+/***/ 75553:
+/***/ (() => {
+
+
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [359,324], () => (__webpack_exec__(20737)));
+module.exports = __webpack_exports__;
+
+})();
